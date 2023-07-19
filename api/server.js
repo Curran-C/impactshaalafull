652234c85c33cf3f394ac4290af815570c3d56d3
= <=>
2023-07-19 11:26:04 +0530

    deployment bug fix 5 added custom domain to render.com backend

--- api/server.js
+++ api/server.js
@@ -25,7 +25,10 @@ const connect = async () => {
 //middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+// app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({ origin: "https://impactshaala-testsite.tech", credentials: true })
+);
 // app.use(cors({ origin: "https://impact-shaala.web.app", credentials: true }));
 
 //routes
