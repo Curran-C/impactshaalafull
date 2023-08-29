2b97567eec4bde2730da493423b7e16b5c99f7ea
= <=>
2023-08-29 16:41:38 +0530

    deployment bug fixes

--- socket/server.js
+++ socket/server.js
@@ -1,6 +1,7 @@
 const io = require("socket.io")(8800, {
   cors: {
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    origin: "http://impactsalla-frontend.s3-website-us-east-1.amazonaws.com",
   },
 });
 
