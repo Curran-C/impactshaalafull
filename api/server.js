38e47f7d2f3a10854e13e52f0740646cb0e974f5
the-capital-hub <141011134+the-capital-hub@users.noreply.github.com>
2023-09-20 13:07:01 +0530

    Updated CORS

--- api/server.js
+++ api/server.js
@@ -32,7 +32,12 @@ const connect = async () => {
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://impactshaala-testsite.tech"],
+    credentials: true,
+  })
+);
 // app.use(cors({ origin: "https://impact-shaala.web.app", credentials: true }));
 // app.use(
 //   cors({
